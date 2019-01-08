473475890a20e86c3574e60042747bd6d01c7c73
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-08 14:44:32 +0100

    primo passaggio per lettura generale del api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  alert("vivo");
+  //salvo in variabilie indirizzo api
+  var urlApi = "http://157.230.17.132:3006/todos";
+  //creo una chiamata ajax per connettermi
+  $.ajax({
+    url: urlApi,
+    //uso get per lettura
+    type: "GET",
+    success: function(data) {
+      //faccio una stampa di prova di tutto il contenuto api
+      console.log(data);
+    },
+    error: function(errore) {
+
+    }
+  })
+
+
 });
