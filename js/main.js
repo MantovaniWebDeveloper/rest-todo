f16606b5afd10abcef818556ebf2ab651c059d71
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-08 16:48:36 +0100

    convertito in nuova funzione la stampa in html di tutta l'api

--- js/main.js
+++ js/main.js
@@ -9,21 +9,26 @@ $(document).ready(function() {
     success: function(data) {
       //faccio una stampa di prova di tutto il contenuto api
       console.log(data);
-      //eseguo il ciclo per stampare
-      for (var i = 0; i < data.length; i++) {
-        //variabilizzo il valore la propieta text di data
-        var testoApi = data[i].text;
-        console.log(testoApi);
-        //resetto
-        $("#listaApi").html();
-        //stamp in html
-        $("#listaApi").append("<li>" + testoApi + "</li>");
-      }
+      stampaApi(data);
     },
     error: function(errore) {
 
     }
   })
+
+  //funzione stampa api
+  function stampaApi(data){
+    //eseguo il ciclo per stampare
+    for (var i = 0; i < data.length; i++) {
+      //variabilizzo il valore la propieta text di data
+      var testoApi = data[i].text;
+      console.log(testoApi);
+      //resetto
+      $("#listaApi").html();
+      //stamp in html
+      $("#listaApi").append("<li>" + testoApi + "</li>");
+    }
+  }
 
 
 });
