83becef1060d8e695bd0e666d3bbfe4f83714a65
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-08 17:45:11 +0100

    stampato id del api nella lista html

--- js/main.js
+++ js/main.js
@@ -1,22 +1,10 @@
 $(document).ready(function() {
   //salvo in variabilie indirizzo api
   var urlApi = "http://157.230.17.132:3006/todos";
-  //creo una chiamata ajax per connettermi e leggere tutto
-  $.ajax({
-    url: urlApi,
-    //uso get per lettura
-    type: "GET",
-    success: function(data) {
-      //faccio una stampa di prova di tutto il contenuto api
-      console.log(data);
-      stampaApi(data);
-    },
-    error: function(errore) {
 
-    }
-  })
 
   //creo una chiamata ajax per connettermi e scrivere nuova nota
+  ajaxGet(urlApi);
 
   $("#btnSalva").click(function(){
       //alert("sono vivo 2");
@@ -30,6 +18,8 @@ $(document).ready(function() {
         success: function(data) {
           //faccio una stampa di prova di tutto il contenuto api
           console.log(data);
+          ajaxGet(urlApi);
+
         },
         error: function(errore) {
 
@@ -40,16 +30,34 @@ $(document).ready(function() {
 
 
   //funzione stampa api
+  function ajaxGet(urlApi){
+    //creo una chiamata ajax per connettermi e leggere tutto
+    $.ajax({
+      url: urlApi,
+      //uso get per lettura
+      type: "GET",
+      success: function(data) {
+        //faccio una stampa di prova di tutto il contenuto api
+        console.log(data);
+        stampaApi(data);
+      },
+      error: function(errore) {
+
+      }
+    })
+  }
   function stampaApi(data){
     //eseguo il ciclo per stampare
     for (var i = 0; i < data.length; i++) {
       //variabilizzo il valore la propieta text di data
       var testoApi = data[i].text;
+      var id = data[i].id;
       console.log(testoApi);
+      console.log(id);
       //resetto
-      $("#listaApi").html();
+      $(".wrapListaApi").html('');
       //stamp in html
-      $("#listaApi").append("<li>" + testoApi + "</li>");
+      $("#listaApi").append("<li id=" + id + ">" + testoApi + "</li>");
     }
   }
 
